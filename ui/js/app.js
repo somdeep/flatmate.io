9925a7c49a11c16fbd447060c5d4e8a7bd9c257c
Matt Mallett <mmallet@us.ibm.com>
2015-12-04 17:59:41 -0500

    user page

--- ui/js/app.js
+++ ui/js/app.js
@@ -10,9 +10,22 @@
       templateUrl: 'templates/profile.html'
     })
     .when('/matches',{
-      templateUrl: 'templates/matches.html'
+      templateUrl: 'templates/matches.html',
+      controller: 'MatchesController',
+      controllerAs: 'matchCtrl'
+    })
+    .when('/users/:id',{
+      templateUrl: 'templates/user.html'
     })
     .otherwise({redirectTo: '/'});
+
+  }])
+
+  .controller('MatchesController', ['$location', function($location){
+
+    this.matchClicked = function(id){
+      $location.url('/users/' + id);
+    }
 
   }]);
 
